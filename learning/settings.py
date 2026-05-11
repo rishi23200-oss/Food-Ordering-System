574d00205c71e1fef666c04c08ffad239f5b6898
Rishikesh Kumar <rishi23200@gmail.com>
2026-05-11 22:02:13 +0530

    fix email backend for deployment

--- learning/settings.py
+++ learning/settings.py
@@ -174,7 +174,7 @@ STATIC_URL = "/static/"
 # EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 from dotenv import load_dotenv
 load_dotenv()
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
